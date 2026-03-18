e9ef2a57a3ea3a38c2a1b25586a19a9070778bca
jostratas <strahinjanovovic23@gmail.com>
2026-03-18 13:26:36 +0100

    Generated the project

---
+++ src/main/java/rs/ac/singidunum/PjIspit2026Application.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjIspit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjIspit2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/singidunum/PjIspit2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjIspit2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
